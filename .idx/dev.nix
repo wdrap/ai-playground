a7d381352447da1a9fc6c6e1ac6ce5d51d978d96
wim.drapier <wim.drapier@gmail.com>
2024-04-30 18:45:54 +0000

    idx config

---
+++ .idx/dev.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.temurin-bin-21
+    pkgs.maven
+  ];
+}
